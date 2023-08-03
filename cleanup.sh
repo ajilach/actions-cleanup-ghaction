c4ddf99579a5a885c615087cbd99f05252941c7f
Dennis Staiger <5349+davosian@users.noreply.github.com>
2023-08-03 19:33:40 +0200

    Limit clean-up logic to GITHUB_WORKSPACE

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,8 @@
 set -o errexit -o nounset -o xtrace -o pipefail
 shopt -s inherit_errexit nullglob dotglob
 
-rm -rf "${HOME:?}"/* "${GITHUB_WORKSPACE:?}"/*
+# rm -rf "${HOME:?}"/* "${GITHUB_WORKSPACE:?}"/*
+rm -rf "${GITHUB_WORKSPACE:?}"/*
 
 if test "${RUNNER_DEBUG:-0}" != '1'; then
   set +o xtrace
